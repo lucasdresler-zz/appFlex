4f6d8e3c2095142f2b841db4f1847fd35f6a8b7c
Wellington LUCAS Lima DRESLER <lucasdresler@live.com>
2020-09-05 23:14:51 -0300

    App Calculadora Flex concluído

---
+++ calcflex.js
@@ -0,0 +1,21 @@
+/**
+ * JS - Aula 05: Calculadora Flex
+ * @author Professor José de Assis
+ */
+
+var etanol, gasolina;
+
+function calcular() {
+    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",", "."));
+    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",", "."));
+
+    if (etanol < 0.7 * gasolina) {
+        document.getElementById("status").src = "etanol.png";
+    } else {
+        document.getElementById("status").src = "gasolina.png";
+    }
+}
+
+function resetar() {
+    document.getElementById("status").src = "neutro.png";
+}
